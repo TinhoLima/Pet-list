850d84343b428a2e0e6bbe0bde63f8b4dafc537f
Alison Coutinho <93990888+TinhoLima@users.noreply.github.com>
2023-10-25 13:44:44 -0300

    udpate

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "../template/Main"
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de React.">
+        {/*  */}
+
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi quod quidem fugit facere sapiente odit ullam dolorum qui a illo! Non similique beatae quae mollitia repudiandae optio maiores numquam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint facilis tenetur reprehenderit doloribus. Laborum, aut delectus accusamus id non nobis, nesciunt excepturi possimus rem repellendus error harum consequatur? Ipsum, nesciunt.</p>
+    </Main>
