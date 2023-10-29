9af571fec7f993689b0f7926aa68ac0a145eaeb7
Alison Coutinho <alisonrj.work@gmail.com>
2023-10-29 10:21:48 -0300

    Update Routes.jsx

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -8,6 +8,6 @@ export default props => (
     <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path='/users' element={<UserCrud />} />
-        <Route path="*" element={<Home />} />
+        <Route path="/*" element={<Home />} />
     </Routes>
 )
